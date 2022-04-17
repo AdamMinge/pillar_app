66ab9c5e0fd19ccbaa4a2e6cd0d372b63ed1326b
adam <minge.adam@gmail.com>
2022-04-17 19:27:37 +0200

    added possibility to move scene

--- include/flow/editor/document/flow/flow_view.h
+++ include/flow/editor/document/flow/flow_view.h
@@ -18,10 +18,21 @@ public:
   void setScene(FlowScene *scene);
   [[nodiscard]] FlowScene *getScene() const;
 
+public Q_SLOTS:
+  void scaleUp();
+  void scaleDown();
+
 protected:
   void drawBackground(QPainter *painter, const QRectF &rect) override;
+  void wheelEvent(QWheelEvent *event) override;
+  void keyPressEvent(QKeyEvent *event) override;
+  void keyReleaseEvent(QKeyEvent *event) override;
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseMoveEvent(QMouseEvent *event) override;
 
 private:
+  QPointF m_mouse_click_pos;
+
   const QColor m_background_color;
   const QColor m_small_grid_color;
   const QColor m_grid_color;
--- src/flow/editor/document/flow/flow_view.cpp
+++ src/flow/editor/document/flow/flow_view.cpp
@@ -1,13 +1,17 @@
+/* ------------------------------------ Qt ---------------------------------- */
+#include <QWheelEvent>
 /* ----------------------------------- Local -------------------------------- */
-#include "flow/editor/document/flow/flow_view.h"
 #include "flow/editor/document/flow/flow_scene.h"
+#include "flow/editor/document/flow/flow_view.h"
 /* -------------------------------------------------------------------------- */
 
 FlowView::FlowView(QWidget *parent)
     : QGraphicsView(parent), m_background_color(53, 53, 53),
       m_small_grid_color(60, 60, 60), m_grid_color(25, 25, 25)
 {
+  setDragMode(QGraphicsView::ScrollHandDrag);
   setRenderHint(QPainter::Antialiasing);
+
   setBackgroundBrush(m_background_color);
 
   setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
@@ -68,4 +72,89 @@ void FlowView::drawBackground(QPainter *painter, const QRectF &rect)
   const auto grid_pen = QPen(m_grid_color, 1.0);
   painter->setPen(grid_pen);
   drawGrid(150);
+}
+
+void FlowView::scaleUp()
+{
+  double const step = 1.2;
+  double const factor = std::pow(step, 1.0);
+
+  QTransform t = transform();
+  if (t.m11() > 2.0) return;
+
+  scale(factor, factor);
+}
+
+void FlowView::scaleDown()
+{
+  double const step = 1.2;
+  double const factor = std::pow(step, -1.0);
+
+  scale(factor, factor);
+}
+
+void FlowView::wheelEvent(QWheelEvent *event)
+{
+  QPoint delta = event->angleDelta();
+  if (delta.y() == 0)
+  {
+    event->ignore();
+    return;
+  }
+
+  double const d = static_cast<double>(delta.y()) / std::abs(delta.y());
+  if (d > 0.0) scaleUp();
+  else
+    scaleDown();
+}
+
+void FlowView::keyPressEvent(QKeyEvent *event)
+{
+  switch (event->key())
+  {
+    case Qt::Key_Shift:
+      setDragMode(QGraphicsView::RubberBandDrag);
+      break;
+
+    default:
+      break;
+  }
+
+  QGraphicsView::keyPressEvent(event);
+}
+
+void FlowView::keyReleaseEvent(QKeyEvent *event)
+{
+  switch (event->key())
+  {
+    case Qt::Key_Shift:
+      setDragMode(QGraphicsView::ScrollHandDrag);
+      break;
+
+    default:
+      break;
+  }
+  QGraphicsView::keyReleaseEvent(event);
+}
+
+void FlowView::mousePressEvent(QMouseEvent *event)
+{
+  QGraphicsView::mousePressEvent(event);
+  if (event->button() == Qt::LeftButton)
+    m_mouse_click_pos = mapToScene(event->pos());
+}
+
+void FlowView::mouseMoveEvent(QMouseEvent *event)
+{
+  QGraphicsView::mouseMoveEvent(event);
+  if (
+    scene()->mouseGrabberItem() == nullptr &&
+    event->buttons() == Qt::LeftButton)
+  {
+    if ((event->modifiers() & Qt::ShiftModifier) == 0)
+    {
+      QPointF difference = m_mouse_click_pos - mapToScene(event->pos());
+      setSceneRect(sceneRect().translated(difference.x(), difference.y()));
+    }
+  }
 }
