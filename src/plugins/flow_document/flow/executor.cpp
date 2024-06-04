84f636f0837675cbfb3a2b59aef5c2c058765635
Adam Minge <minge.adam@gmail.com>
2024-06-04 09:16:53 +0200

    Remove order delete of singletons

---
+++ src/plugins/flow_document/flow/executor.cpp
@@ -0,0 +1,11 @@
+/* ----------------------------------- Local -------------------------------- */
+#include "flow_document/flow/executor.h"
+/* -------------------------------------------------------------------------- */
+
+namespace flow_document {
+
+Executor::Executor() = default;
+
+Executor::~Executor() = default;
+
+}  // namespace flow_document
