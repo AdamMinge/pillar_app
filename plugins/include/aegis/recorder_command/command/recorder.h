1c31ff9f57b211f70399f16481957224059034df
Adam Minge <minge.adam@gmail.com>
2024-07-26 23:25:52 +0200

    add simple record command

--- plugins/include/aegis/recorder_command/command/recorder.h
+++ plugins/include/aegis/recorder_command/command/recorder.h
@@ -11,7 +11,47 @@
 #include "aegis/recorder_command/export.h"
 /* -------------------------------------------------------------------------- */
 
+class QMouseEvent;
+class QKeyEvent;
+class QWheelEvent;
+
 namespace aegis {
+
+namespace event {
+
+struct MouseButtonEvent {
+  QPoint pos;
+  Qt::MouseButton button;
+  Qt::MouseButtons buttons;
+  Qt::KeyboardModifiers modifiers;
+};
+
+struct MouseButtonPressEvent : public MouseButtonEvent {};
+
+struct MouseButtonReleaseEvent : public MouseButtonEvent {};
+
+struct MouseMoveEvent {
+  QPoint pos;
+  Qt::MouseButtons buttons;
+  Qt::KeyboardModifiers modifiers;
+};
+
+struct MouseWheelEvent {
+  QPoint angle;
+  Qt::KeyboardModifiers modifiers;
+};
+
+struct KeyEvent {
+  int key;
+  Qt::KeyboardModifiers modifiers;
+  QString text;
+};
+
+struct KeyPressEvent : public KeyEvent {};
+
+struct KeyReleaseEvent : public KeyEvent {};
+
+}  // namespace event
 
 /* ---------------------------------- Recorder ------------------------------ */
 
@@ -20,9 +60,11 @@ class LIB_AEGIS_RECORDER_COMMAND_API Recorder : public QObject {
 
  public:
   using Result = Response<>;
+  enum class State { Running, Paused, Stopped };
 
  public:
-  explicit Recorder(QObject *parent = nullptr);
+  explicit Recorder(QObject* parent = nullptr);
+  ~Recorder();
 
   Result start();
   Result pause();
@@ -30,21 +72,47 @@ class LIB_AEGIS_RECORDER_COMMAND_API Recorder : public QObject {
   Result report();
 
  protected:
-  bool eventFilter(QObject *obj, QEvent *event) override;
+  bool eventFilter(QObject* obj, QEvent* event) override;
 
  private:
+  void recordMouseButtonPress(QObject* obj, QMouseEvent* event);
+  void recordMouseButtonRelease(QObject* obj, QMouseEvent* event);
+  void recordMouseMove(QObject* obj, QMouseEvent* event);
+  void recordMouseWheel(QObject* obj, QWheelEvent* event);
+
+  void recordKeyPress(QObject* obj, QKeyEvent* event);
+  void recordKeyRelease(QObject* obj, QKeyEvent* event);
+
+  template <typename TYPE>
+  requires std::is_rvalue_reference_v<TYPE&&>
+  void recordEvent(TYPE&& event);
+
+ private:
+  State m_state;
+  QList<QVariant> m_events;
 };
+
+template <typename TYPE>
+requires std::is_rvalue_reference_v<TYPE&&>
+void Recorder::recordEvent(TYPE&& event) {
+  auto variant = QVariant::fromValue(std::forward<TYPE>(event));
+  if (m_events.size() > 0 && m_events.back().canConvert<TYPE>()) {
+    m_events.back() = std::move(variant);
+  } else {
+    m_events.push_back(std::move(variant));
+  }
+}
 
 /* ------------------------------ RecorderCommand --------------------------- */
 
 class LIB_AEGIS_RECORDER_COMMAND_API RecorderCommand : public Command {
  public:
-  explicit RecorderCommand(const ResponseSerializer &serializer);
+  explicit RecorderCommand(const ResponseSerializer& serializer);
   ~RecorderCommand() override;
 
   [[nodiscard]] QString getName() const override;
 
-  [[nodiscard]] QByteArray exec(const QStringList &args) override;
+  [[nodiscard]] QByteArray exec(const QStringList& args) override;
 
  private:
   QCommandLineParser m_parser;
@@ -52,5 +120,12 @@ class LIB_AEGIS_RECORDER_COMMAND_API RecorderCommand : public Command {
 };
 
 }  // namespace aegis
+
+Q_DECLARE_METATYPE(aegis::event::MouseButtonPressEvent)
+Q_DECLARE_METATYPE(aegis::event::MouseButtonReleaseEvent)
+Q_DECLARE_METATYPE(aegis::event::MouseMoveEvent)
+Q_DECLARE_METATYPE(aegis::event::MouseWheelEvent)
+Q_DECLARE_METATYPE(aegis::event::KeyPressEvent)
+Q_DECLARE_METATYPE(aegis::event::KeyReleaseEvent)
 
 #endif  // AEGIS_SERVER_COMMAND_RECORDER_H
--- plugins/src/aegis/recorder_command/command/recorder.cpp
+++ plugins/src/aegis/recorder_command/command/recorder.cpp
@@ -2,29 +2,150 @@
 #include "aegis/recorder_command/command/recorder.h"
 /* ------------------------------------ Qt ---------------------------------- */
 #include <QApplication>
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QWheelEvent>
 /* -------------------------------------------------------------------------- */
 
 namespace aegis {
 
+static constexpr QLatin1String recorder_error =
+    QLatin1String("Recorder Command Error");
+
 /* ---------------------------------- Recorder ------------------------------ */
 
-Recorder::Recorder(QObject *parent) : QObject(parent) {}
+Recorder::Recorder(QObject* parent) : QObject(parent), m_state(State::Stopped) {
+  auto app = QApplication::instance();
+  moveToThread(app->thread());
+}
 
-Recorder::Result Recorder::start() { return EmptyMessage(); }
+Recorder::~Recorder() = default;
 
-Recorder::Result Recorder::pause() { return EmptyMessage(); }
+Recorder::Result Recorder::start() {
+  if (m_state == State::Running) {
+    return ErrorMessage(
+        recorder_error,
+        QLatin1String(
+            "The start cannot be triggered, the recorder is already working"));
+  }
 
-Recorder::Result Recorder::stop() { return EmptyMessage(); }
+  auto app = QApplication::instance();
+  app->installEventFilter(this);
+  m_state = State::Running;
+
+  return EmptyMessage();
+}
+
+Recorder::Result Recorder::pause() {
+  if (m_state != State::Running) {
+    return ErrorMessage(
+        recorder_error,
+        QLatin1String(
+            "The pause cannot be triggered, the recorder is not working"));
+  }
+
+  auto app = QApplication::instance();
+  app->removeEventFilter(this);
+  m_state = State::Paused;
+
+  return EmptyMessage();
+}
+
+Recorder::Result Recorder::stop() {
+  if (m_state == State::Stopped) {
+    return ErrorMessage(
+        recorder_error,
+        QLatin1String(
+            "The stop cannot be triggered, the recorder is already stopped"));
+  }
+
+  auto app = QApplication::instance();
+  app->removeEventFilter(this);
+  m_state = State::Stopped;
+  m_events.clear();
+
+  return EmptyMessage();
+}
 
 Recorder::Result Recorder::report() { return EmptyMessage(); }
 
-bool Recorder::eventFilter(QObject *obj, QEvent *event) {
+bool Recorder::eventFilter(QObject* obj, QEvent* event) {
+  auto event_type = event->type();
+
+  if (event_type == QEvent::MouseButtonPress)
+    recordMouseButtonPress(obj, static_cast<QMouseEvent*>(event));
+  else if (event_type == QEvent::MouseButtonRelease)
+    recordMouseButtonRelease(obj, static_cast<QMouseEvent*>(event));
+  else if (event_type == QEvent::MouseMove)
+    recordMouseMove(obj, static_cast<QMouseEvent*>(event));
+  else if (event_type == QEvent::Wheel)
+    recordMouseWheel(obj, static_cast<QWheelEvent*>(event));
+  else if (event_type == QEvent::KeyPress)
+    recordKeyPress(obj, static_cast<QKeyEvent*>(event));
+  else if (event_type == QEvent::KeyRelease)
+    recordKeyRelease(obj, static_cast<QKeyEvent*>(event));
+
   return QObject::eventFilter(obj, event);
+}
+
+void Recorder::recordMouseButtonPress(QObject* obj, QMouseEvent* event) {
+  auto record_event = event::MouseButtonPressEvent{};
+  record_event.pos = event->pos();
+  record_event.button = event->button();
+  record_event.buttons = event->buttons();
+  record_event.modifiers = event->modifiers();
+
+  recordEvent(std::move(record_event));
+}
+
+void Recorder::recordMouseButtonRelease(QObject* obj, QMouseEvent* event) {
+  auto record_event = event::MouseButtonReleaseEvent{};
+  record_event.pos = event->pos();
+  record_event.button = event->button();
+  record_event.buttons = event->buttons();
+  record_event.modifiers = event->modifiers();
+
+  recordEvent(std::move(record_event));
+}
+
+void Recorder::recordMouseMove(QObject* obj, QMouseEvent* event) {
+  auto record_event = event::MouseMoveEvent{};
+  record_event.pos = event->pos();
+  record_event.buttons = event->buttons();
+  record_event.modifiers = event->modifiers();
+
+  recordEvent(std::move(record_event));
+}
+
+void Recorder::recordMouseWheel(QObject* obj, QWheelEvent* event) {
+  auto record_event = event::MouseWheelEvent{};
+  record_event.angle = event->angleDelta();
+  record_event.modifiers = event->modifiers();
+
+  recordEvent(std::move(record_event));
+}
+
+void Recorder::recordKeyPress(QObject* obj, QKeyEvent* event) {
+  auto record_event = event::KeyPressEvent{};
+  record_event.key = event->key();
+  record_event.modifiers = event->modifiers();
+  record_event.text = event->text();
+
+  recordEvent(std::move(record_event));
+}
+
+void Recorder::recordKeyRelease(QObject* obj, QKeyEvent* event) {
+  auto record_event = event::KeyReleaseEvent{};
+  record_event.key = event->key();
+  record_event.modifiers = event->modifiers();
+  record_event.text = event->text();
+
+  recordEvent(std::move(record_event));
 }
 
 /* ------------------------------ RecorderCommand --------------------------- */
 
-RecorderCommand::RecorderCommand(const ResponseSerializer &serializer)
+RecorderCommand::RecorderCommand(const ResponseSerializer& serializer)
     : Command(serializer) {
   m_parser.addHelpOption();
   m_parser.addOptions({
@@ -40,7 +161,7 @@ RecorderCommand::~RecorderCommand() = default;
 
 QString RecorderCommand::getName() const { return QString("Recorder"); }
 
-QByteArray RecorderCommand::exec(const QStringList &args) {
+QByteArray RecorderCommand::exec(const QStringList& args) {
   if (m_parser.parse(args)) {
     if (m_parser.isSet("start")) return serialize(m_recorder.start());
     if (m_parser.isSet("pause")) return serialize(m_recorder.pause());
@@ -48,8 +169,7 @@ QByteArray RecorderCommand::exec(const QStringList &args) {
     if (m_parser.isSet("report")) return serialize(m_recorder.report());
   }
 
-  auto error = Response<>(ErrorMessage(QLatin1String("Recorder Command Error"),
-                                       m_parser.errorText()));
+  auto error = Response<>(ErrorMessage(recorder_error, m_parser.errorText()));
   return serialize(error);
 }
 
